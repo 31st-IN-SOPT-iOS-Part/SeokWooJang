8f4174fccfab9ae4b18498cca27f5a54eeb96dca
Melt <seokwoo2000@naver.com>
2022-10-07 00:37:28 +0900

    [Feat] Extension 추가 #1

--- IN SOPT KakaoTalk/IN SOPT KakaoTalk/Source/SignInViewController.swift
+++ IN SOPT KakaoTalk/IN SOPT KakaoTalk/Source/SignInViewController.swift
@@ -1,19 +1,18 @@
 //
-//  ViewController.swift
-//  IN SOPT KakaoTalk
+//  SignInViewController.swift
+//  1st Assignment
 //
-//  Created by 장석우 on 2022/10/06.
+//  Created by 장석우 on 2022/10/07.
 //
 
 import UIKit
 
 class SignInViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .yellow
+        
+        view.backgroundColor = .kakaoYellow
     }
 
-
 }
-
